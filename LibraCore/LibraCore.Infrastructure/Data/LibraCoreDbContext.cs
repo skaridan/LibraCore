df795867834982345f72e1183963aab09418c2e9
Martin Atanasov <martinatanasov156@gmail.com>
2026-04-01 18:23:30 +0300

    Initialize project structure and project references.

---
+++ LibraCore/LibraCore.Infrastructure/Data/LibraCoreDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraCore.Infrastructure.Data
+{
+    public class LibraCoreDbContext : IdentityDbContext
+    {
+        public LibraCoreDbContext(DbContextOptions<LibraCoreDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
