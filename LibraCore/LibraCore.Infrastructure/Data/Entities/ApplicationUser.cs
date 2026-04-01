6721844548b87cd19dbedf1b7fe2398de91c32b3
Martin Atanasov <martinatanasov156@gmail.com>
2026-04-01 18:46:40 +0300

    Initialzie ApplicationUser.

---
+++ LibraCore/LibraCore.Infrastructure/Data/Entities/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LibraCore.Infrastructure.Data.Entities
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public ApplicationUser()
+        {
+            this.Id = Guid.NewGuid();
+        }
+    }
+}
--- LibraCore/LibraCore.Infrastructure/Data/LibraCoreDbContext.cs
+++ LibraCore/LibraCore.Infrastructure/Data/LibraCoreDbContext.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LibraCore.Infrastructure.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LibraCore.Infrastructure.Data
 {
-    public class LibraCoreDbContext : IdentityDbContext
+    public class LibraCoreDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
         public LibraCoreDbContext(DbContextOptions<LibraCoreDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
