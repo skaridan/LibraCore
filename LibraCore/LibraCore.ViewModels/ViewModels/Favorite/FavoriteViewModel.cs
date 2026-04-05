a0059704029f3d5016f3a0d27e21868bf686c0e7
Martin Atanasov <martinatanasov156@gmail.com>
2026-04-05 22:04:17 +0300

    Add Favorite css and Implementation of FavoriteController Action Index

---
+++ LibraCore/LibraCore.ViewModels/ViewModels/Favorite/FavoriteViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraCore.ViewModels
+{
+    public class FavoriteViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Author { get; set; } = null!;
+
+        public string? ImageUrl { get; set; }
+    }
+}
---
+++ LibraCore/LibraCore.Web/Controllers/Favorite/FavoriteController.cs
@@ -0,0 +1,28 @@
+﻿using LibraCore.Services.Services;
+using LibraCore.Services.Services.Interfaces;
+using LibraCore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraCore.Web.Controllers.Favorite
+{
+    public class FavoriteController : BaseController
+    {
+        private readonly IFavoriteService favoriteService;
+
+        public FavoriteController(IFavoriteService favoriteService)
+        {
+            this.favoriteService = favoriteService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            string userId = GetUserId()!;
+
+            IEnumerable<FavoriteViewModel> favoriteViewModels = await favoriteService
+                .GetUserBooksOrderedByTitleAsync(userId);
+
+            return View(favoriteViewModels);
+        }
+    }
+}
